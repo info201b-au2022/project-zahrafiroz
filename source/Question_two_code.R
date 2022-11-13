20936a0eb4b25dcf0eb1bf98eee13010e6a85be5
IanHWUW <ianhwpaul@gmail.com>
2022-11-13 15:18:34 -0800

    q2 code file

---
+++ source/Question_two_code.R
@@ -0,0 +1,52 @@
+library("dplyr")
+install.packages("ggplot2")
+library("ggplot2")
+
+Frequency_of_distasters <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-zahrafiroz/main/data/Climate-related_Disasters_Frequency.csv")
+View(Frequency_of_distasters)
+Cities_climate_hazards <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-zahrafiroz/main/data/2022_Cities_Climate_Hazards.csv")
+View(Cities_climate_hazards)
+
+Disaster_Years_Com
+Distaster_Years_Combined <- mutate(Distaster_Years_Combined, "1980" = F1980)
+
+Highest_frequency <- Frequency_of_distasters %>%
+  group_by(Country) %>%
+  mutate(total = sum(across(starts_with("F")), na.rm = T)) %>%
+  select(Country, total) %>%
+  arrange(-total)
+Highest_frequency <- distinct(Highest_frequency)
+  View(Highest_frequency)
+
+Top_ten_countries <- head(Highest_frequency, 10)
+Top_ten_countries
+
+ggplot(data = Top_ten_countries) + 
+  geom_col(
+             mapping = aes( 
+               x = reorder(Top_ten_countries$Country,
+                           + Top_ten_countries$total),
+               y = Top_ten_countries$total,
+               color = Country,
+               fill = Country)
+  )
+
+
+Highest_Percent_Vulnurable <- Cities_climate_hazards %>%
+  group_by(Proportion.of.population.exposed.to.hazard) %>%
+  filter(Proportion.of.population.exposed.to.hazard == "90-100%") %>%
+  filter(Current.magnitude.of.impact.of.hazard == "High") %>%
+  filter(Current.probability.of.hazard == "High") %>%
+  filter(Did.this.hazard.significantly.impact.your.jurisdiction.before.this.reporting.year. == "Yes") %>%
+  pull(City, Country)
+View(Highest_Percent_Vulnurable)
+
+Most_Vulnerable <- Highest_Percent_Vulnurable %>%
+  filter(Population == max(Population)) %>%
+  pull(City, Country)
+Most_Vulnerable
+
+# Lagos, Nigeria
+
+
+
