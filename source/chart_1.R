5cdf420b15d36247dadfb483a3196473b480774e
tkl22-1961346 <81778081+tkl22-1961346@users.noreply.github.com>
2022-11-14 13:24:45 -0800

    edit path

--- source/chart_1.R
+++ source/chart_1.R
@@ -6,9 +6,9 @@ library("plotly")
 # Question: How has human health been impacted?
 #
 # Topic: Top hazards across countries
-# 
+#
 #   Loading data
-climate_hazards <- read.csv("~/Documents/info201/project-zahrafiroz/data/2022_Cities_Climate_Hazards.csv")
+climate_hazards <- read.csv("../data/2022_Cities_Climate_Hazards.csv")
 
 #   Narrowing down columns
 climate_hazards <- climate_hazards %>%
@@ -24,8 +24,8 @@ hazards_count <- climate_hazards %>%
   top_n(20)
 
 hazards_count <- data.frame(
-  hazards=hazards_count$Climate.related.hazards,
-  number_of_occurrences=hazards_count$n
+  hazards = hazards_count$Climate.related.hazards,
+  number_of_occurrences = hazards_count$n
 )
 
 #   Graphing
@@ -33,15 +33,15 @@ hazards_chart <- plot_ly(
   data = hazards_count,
   labels = ~hazards,
   values = ~number_of_occurrences,
-  type = "pie",  
-  textposition = "inside",  
+  type = "pie",
+  textposition = "inside",
   textinfo = "label+percent",
   hoverinfo = "label+text",
-  text = ~paste(number_of_occurrences, "Cities Affected"),
+  text = ~ paste(number_of_occurrences, "Cities Affected"),
   insidetextfont = list(color = "#FFFFFF"),
   marker = list(line = list(color = "#FFFFFF", width = 0.7)),
   showlegend = TRUE
 ) %>%
   layout(
     title = "Top 20 Climate Related Hazards in 2022"
-)
+  )
