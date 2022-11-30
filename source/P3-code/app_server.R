a3ff75bb356bd695624376777d6aa36e115ed7bb
CalvinUW <calvinst@uw.edu>
2022-11-30 13:51:21 -0800

    Work on gdp section

--- source/P3-code/app_server.R
+++ source/P3-code/app_server.R
@@ -1,19 +1,51 @@
 
 library(shiny)
+library(tidyverse)
+source("./data_wrangling/tab_3_data_wrangling.R")
+
 
 server <- function(input, output) {
 
+  get_gdp_color <- reactive({
+    if(input$color_gdp == "country") {
+      list("blue", "cyan", "red", "yellow", "magenta", "green", "orange", "olive")
+    } else {
+      ~gdp_and_disasters[, input$color_gdp]
+    }
+  })
+  
   output$gdp_chart <- renderPlotly({
-    plot_data <- gdp_and_disasters
     plot_ly(
-      data = plot_data,
-      x = ~plot_data[, input$gdp_chart_options],
+      data = gdp_and_disasters,
+      x = ~ gdp_and_disasters[, input$gdp_chart_options],
       y = ~avg_gdp,
       type = "scatter",
       mode = "markers",
+      color = get_gdp_color(),
       hoverinfo = "text",
-      hovertext = paste0(plot_data$country, "<br>",
-                         plot_data[[input$gdp_chart_options]])
+      hovertext = paste0(
+        "Country: ", gdp_and_disasters$country, "<br>",
+        input$gdp_chart_options, " Disasters: ",
+        gdp_and_disasters[[input$gdp_chart_options]], "<br>",
+        "Avg. GDP% Spent on Environmental Protection: ",
+        round(gdp_and_disasters$avg_gdp, 2), "%", "<br>",
+        if (input$color_gdp != "country") {
+          paste0(str_to_title(input$color_gdp), ": ", gdp_and_disasters[[input$color_gdp]])
+        }
+      )
+    ) %>% layout(
+      title = "Disasters vs. Avg. GDP% Spent on Environmental Protection (1980-2021)",
+      xaxis = list(
+        title = paste(
+          "Count of", str_to_title(input$gdp_chart_options),
+          "Disasters Impacting Nation"
+        ),
+        ticksuffix = ""
+      ),
+      yaxis = list(
+        title = "Avg GDP% Spent on Environmental Protection",
+        ticksuffix = ""
+      )
     )
   })
 }
--- source/P3-code/tabs/tab_panel_chart3.R
+++ source/P3-code/tabs/tab_panel_chart3.R
@@ -1,23 +1,6 @@
 # tab_panel_chart3
-library("tidyverse")
-library("ggplot2")
-library("plotly")
-library(countrycode)
 library(shiny)
-
-disaster_freq <- read.csv("../../data/Climate-related_Disasters_Frequency.csv",
-                          stringsAsFactors = FALSE
-)
-
-gov_expend <- read.csv("../../data/Environmental_Protection_Expenditures.csv",
-                       stringsAsFactors = FALSE
-)
-
-tab_choices <- list("Total Disasters" = "TOTAL", "Droughts" = "Drought",
-                    "Wildfires" = "Wildfire", "Floods" = "Flood",
-                    "Extreme Temp. Events" = "Extreme temperature",
-                    "Storms" = "Storm", "Landslides" = "Landslide")
-
+source('./data_wrangling/tab_3_data_wrangling.R')
 
 tab_panel_chart3 <- tabPanel(
   "Chart 3",
@@ -29,6 +12,14 @@ tab_panel_chart3 <- tabPanel(
         label = "Disaster Categories",
         choices = tab_choices,
         selected = "Total Disasters"
+      ),
+      
+      radioButtons(
+        inputId = "color_gdp",
+        label = "Color",
+        choices = list("Country" = "country", "Region" = "region",
+                       "Continent" = "continent"),
+        selected = NULL
       )
     ),
     mainPanel(
@@ -39,37 +30,3 @@ tab_panel_chart3 <- tabPanel(
   )
 )
 
-#Actual code, not sure if this is meant to be here
-  
-
-
-# Find the number of climate related disasters that have affected nations.
-disaster_freq_nations <- disaster_freq %>% 
-  group_by(ISO3, Indicator) %>% 
-  mutate(sum = sum(across(starts_with("F")), na.rm = TRUE)) %>% 
-  spread(key = Indicator, value = sum) %>% 
-  group_by(ISO3) %>% 
-  summarize(across(starts_with("Climate related"), sum, na.rm=T)) %>% 
-  rename_with(~gsub(".*: ", "", .), starts_with("Climate related"))
-
-# Find the average percent of gdp nations spend on environmental protection.
-avg_gdp_nations <- gov_expend %>%
-  filter(Unit == "Percent of GDP") %>%
-  group_by(ISO3) %>%
-  summarise(sum = sum(across(starts_with("F")), na.rm = TRUE)) %>%
-  mutate(avg_gdp = sum / ncol(select(gov_expend, starts_with("F")))) %>%
-  select(-sum)
-
-gdp_and_disasters <- left_join(avg_gdp_nations, testing, by = "ISO3") %>% 
-  filter(ISO3 != "") %>% 
-  mutate(across(where(is.numeric), replace_na, 0)) %>% 
-  as.data.frame()
-
-gdp_and_disasters$country <- countrycode(sourcevar = gdp_and_disasters[, "ISO3"],
-                                         origin = "iso3c",
-                                         destination = "country.name")
-
-
-
-
-  
